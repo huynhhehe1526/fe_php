a14b767381d4a6681cf0a96f394ef1f5617c2bd9
Huỳnh <dangnhuh2003@gmail.com>
2025-04-16 15:30:47 +0700

    fix axios

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,9 @@ import _ from 'lodash';
 
 const instance = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URL,
+    headers: {
+        'Content-Type': 'application/json',
+      },
     // withCredentials: true
 });
 
